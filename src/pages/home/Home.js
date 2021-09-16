ba9726dc6fc8578e7ee1362437f83dd4b1be23dd
Delia Constantino <dconstan92@gmail.com>
2021-09-16 12:51:34 -0400

    created Home and Teleport components with Router Links

---
+++ src/pages/home/Home.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div>
+      <Link to="/teleport">Teleport</Link>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/pages/teleport/Teleport.js
@@ -0,0 +1,11 @@
+function Teleport() {
+  return (
+    <div>
+      <p>breadcrumbs will go here</p>
+      <p>search box here</p>
+      <p>Top level teleport table will go here</p>
+    </div>
+  )
+}
+
+export default Teleport;
