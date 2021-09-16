5cd19112f573408e97781f30fc3d3414d27b4595
Delia Constantino <dconstan92@gmail.com>
2021-09-16 15:30:03 -0400

    add directory data to frontend

---
+++ src/data/directory.js
@@ -0,0 +1,29 @@
+export const directory = {
+  name: "teleport",
+  sizeKb: 0,
+  type: "dir",
+  items: [
+    {
+      name: "lib",
+      sizeKb: 0,
+      type: "dir",
+      items: [
+        {
+          name: "teleport.go",
+          sizeKb: 320,
+          type: "file",
+        },
+        {
+          name: "test.go",
+          sizeKb: 3320,
+          type: "file",
+        },
+      ],
+    },
+    {
+      name: "README.md",
+      sizeKb: 4340,
+      type: "file",
+    },
+  ],
+};
--- src/pages/teleport/Teleport.js
+++ src/pages/teleport/Teleport.js
@@ -1,11 +1,15 @@
 import Table from "../../components/table/Table.js"
+import { directory } from "../../data/directory.js"
+
 function Teleport() {
+  console.log(directory)
   return (
     <div>
       <p>breadcrumbs will go here</p>
       <p>search box here</p>
       <p>Top level teleport table will go here</p>
       <Table />
+
     </div>
   );
 }
