a6b7aa4aec2b7d9cf6e824c0eba017ff5f422fd5
Delia Constantino <dconstan92@gmail.com>
2021-09-16 15:26:17 -0400

    create Table component and render from page

---
+++ src/components/table/Table.js
@@ -0,0 +1,28 @@
+function Table () {
+  return (
+    <table className="">
+      <thead>
+        <tr>
+          <th className="">Name</th>
+          <th className="">Size</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td className="">Indiana</td>
+          <td className="">25</td>
+        </tr>
+        <tr>
+          <td className="">Ohio</td>
+          <td className="">300</td>
+        </tr>
+        <tr>
+          <td className="">Michigan</td>
+          <td className="">430</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
+
+export default Table;
--- src/pages/teleport/Teleport.js
+++ src/pages/teleport/Teleport.js
@@ -1,9 +1,11 @@
+import Table from "../../components/table/Table.js"
 function Teleport() {
   return (
     <div>
       <p>breadcrumbs will go here</p>
       <p>search box here</p>
       <p>Top level teleport table will go here</p>
+      <Table />
     </div>
   );
 }
