dd97a10ad0582b72aa28c27f9bed9f94b976f52e
Delia Constantino <dconstan92@gmail.com>
2021-09-16 15:43:45 -0400

    add unnamed file/dir column to Table

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -3,20 +3,24 @@ function Table () {
     <table className="">
       <thead>
         <tr>
+          <th className=""></th>
           <th className="">Name</th>
           <th className="">Size</th>
         </tr>
       </thead>
       <tbody>
         <tr>
+          <td className="">file</td>
           <td className="">Indiana</td>
           <td className="">25</td>
         </tr>
         <tr>
+          <td className="">file</td>
           <td className="">Ohio</td>
           <td className="">300</td>
         </tr>
         <tr>
+          <td className="">dir</td>
           <td className="">Michigan</td>
           <td className="">430</td>
         </tr>
