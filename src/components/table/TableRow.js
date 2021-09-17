db4b15aabfade2e5a208264899041652e9ba67ce
Delia Constantino <dconstan92@gmail.com>
2021-09-17 15:51:18 -0400

    fix routing based on url

--- src/components/table/TableRow.js
+++ src/components/table/TableRow.js
@@ -1,8 +1,26 @@
-function TableRow({ type, name, size }) {
+import { Link, useRouteMatch, Switch, Route } from "react-router-dom";
+import DataFinder from "../DataFinder.js"
+
+function TableRow({ type, name, size, items }) {
+  let { url } = useRouteMatch()
+  console.log("row", type, name, size, items)
+  console.log(url)
+
+  let nameValue;
+  if (type === "dir") {
+    nameValue =  (
+      <div>
+        <Link to={`${url}${name}`}>{name}</Link>
+      </div>
+    )
+  } else {
+    nameValue = name
+  }
+
   return (
     <tr>
       <td>{type}</td>
-      <td>{name}</td>
+      <td>{nameValue}</td>
       <td>{size}</td>
     </tr>
   );
