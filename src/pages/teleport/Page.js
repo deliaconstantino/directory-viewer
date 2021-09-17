bbc305e8e782738869aaaab6fbb2fa871d3ade16
Delia Constantino <dconstan92@gmail.com>
2021-09-17 13:46:03 -0400

    move page outline into Page component

--- src/pages/teleport/Page.js
+++ src/pages/teleport/Page.js
@@ -3,22 +3,24 @@ import { directory } from "../../data/directory.js";
 import { Link, useRouteMatch } from "react-router-dom";
 
 function Page({ data }) {
-  console.log("data", data);
+  console.log("data - page", data);
   let { url } = useRouteMatch();
+  // const listItems = directory?.items?.map((val, idx) => {
+  //   console.log("i", idx, "current value", val);
 
-  const listItems = directory?.items?.map((val, idx) => {
-    console.log("i", idx, "current value", val);
-
-    return (
-      <li key={idx}>
-        <Link to={`${url}/${val.name}`}>{val.name}</Link>
-      </li>
-    );
-  });
+  //   return (
+  //     <li key={idx}>
+  //       <Link to={`${url}/${val.name}`}>{val.name}</Link>
+  //     </li>
+  //   );
+  // });
 
   return (
     <div>
-      <ul>{listItems}</ul>
+      <p>breadcrumbs - {url}</p>
+      <p>search box here</p>
+      <p>Top level teleport table will go here</p>
+      {/* <ul>{listItems}</ul> */}
     </div>
   );
 }
