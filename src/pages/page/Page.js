f231ad590d442662397fd45bf5e2afc08662970f
Delia Constantino <dconstan92@gmail.com>
2021-09-17 15:52:19 -0400

    remove comments

--- src/pages/page/Page.js
+++ src/pages/page/Page.js
@@ -1,26 +1,20 @@
 import Table from "../../components/table/Table.js";
-import BreadCrumbs from "./components/BreadCrumbs.js"
+import BreadCrumbs from "./components/BreadCrumbs.js";
 import { directory } from "../../data/directory.js";
 import { Link, useRouteMatch } from "react-router-dom";
 
 function Page({ data }) {
   console.log("data - page", data);
-  // const listItems = directory?.items?.map((val, idx) => {
-  //   console.log("i", idx, "current value", val);
 
-  //   return (
-  //     <li key={idx}>
-  //       <Link to={`${url}/${val.name}`}>{val.name}</Link>
-  //     </li>
-  //   );
-  // });
+  //render a table on the page, each table row = a directory
 
   return (
     <div>
       <BreadCrumbs />
       <p>search box here</p>
       <p>Top level teleport table will go here</p>
-      {/* <ul>{listItems}</ul> */}
+
+      <Table data={data} />
     </div>
   );
 }
