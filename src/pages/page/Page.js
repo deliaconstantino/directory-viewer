2fd78816ec76372ba5938286e6d47e4d39c83386
Delia Constantino <dconstan92@gmail.com>
2021-09-19 17:36:30 -0400

    remove unused code and add location to props

--- src/pages/page/Page.js
+++ src/pages/page/Page.js
@@ -1,20 +1,12 @@
 import Table from "../../components/table/Table.js";
 import BreadCrumbs from "./components/BreadCrumbs.js";
-import { directory } from "../../data/directory.js";
-import { Link, useRouteMatch } from "react-router-dom";
 
-function Page({ data }) {
-  console.log("data - page", data);
-
-  //render a table on the page, each table row = a directory
-
+function Page({ data, location }) {
   return (
     <div>
       <BreadCrumbs />
       <p>search box here</p>
-      <p>Top level teleport table will go here</p>
-
-      <Table data={data} />
+      <Table data={data} location={location} />
     </div>
   );
 }
