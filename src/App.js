42080fe60767bcd6a690d713ae960f8ae9d438b3
Delia Constantino <dconstan92@gmail.com>
2021-09-19 17:32:11 -0400

    update App to render DataFinder component on all route possibilites

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
-import Teleport from "./pages/page/Teleport.js";
-import Home from "./pages/home/Home.js";
-import Page from "./pages/page/Page.js";
 import DataFinder from "./components/DataFinder.js";
 import { directory } from "./data/directory.js";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <div className="bg-indigo-200 shadow-md">
-        <Link className="underline" to="/">
-          Directory Viewer
-        </Link>
-        <Link className="underline" to={`${directory.name}`}>
-          Teleport
-        </Link>
-      </div>
-
       <Switch>
-        <Route path="/:path">
-          <DataFinder />
+        <Route path="/directory/:id">
+          <DataFinder/>
+        </Route>
+        <Route path="/directory">
+          <DataFinder/>
         </Route>
         <Route path="/">
-          <Home />
+          <Redirect to="/directory" />
         </Route>
       </Switch>
     </Router>
