2b6fd42efe8408b0a8cab949e574fa897e4bbe1c
Delia Constantino <dconstan92@gmail.com>
2021-09-20 19:21:27 -0400

    fix spacing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import DataFinder from "./components/DataFinder.js";
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="container mx-auto m-10">
-      <h1 className="font-bold text-3xl text-gray-700">
-        Directory Viewer
-      </h1>
+      <h1 className="font-bold text-3xl text-gray-700">Directory Viewer</h1>
       <Router>
-      <Switch>
-        <Route path="/directory">
-          <DataFinder/>
-        </Route>
-        <Route path="/">
-          <Redirect to="/directory" />
-        </Route>
-      </Switch>
-    </Router>
+        <Switch>
+          <Route path="/directory">
+            <DataFinder />
+          </Route>
+          <Route path="/">
+            <Redirect to="/directory" />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
