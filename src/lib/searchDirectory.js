3b92ce85579e53e373a4ae860981b2a9ec8d268f
Delia Constantino <dconstan92@gmail.com>
2021-09-20 19:04:30 -0400

    add tests to check whether data is correctly found based on url

--- src/lib/searchDirectory.js
+++ src/lib/searchDirectory.js
@@ -1,8 +1,6 @@
 import { directory } from "../data/directory.js";
 
 export function searchDirectory(urlArray) {
-    //TODO: add check that this value exists in this directory
-  // if (urlArray[0] !== "directory") return false;
   let parentLevel = directory;
   if (urlArray.length === 1) {
     return parentLevel;
@@ -18,10 +16,12 @@ export function searchDirectory(urlArray) {
   let foundIdx;
   for (let i = 2; i < urlArray.length; i++) {
     foundIdx = parentLevel.findIndex((val) => val.name === urlArray[i]);
-    if (foundIdx > -1) parentLevel = parentLevel[foundIdx].items;
+    if (foundIdx > -1) {
+      parentLevel = parentLevel[foundIdx].items;
+    } else {
+      return false;
+    }
   }
-
-  if (foundIdx === -1) return false;
 
   return parentLevel;
 }
