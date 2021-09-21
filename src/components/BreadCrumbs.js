995ee86cd2a8d636decbb097bf777ffce339f570
Delia Constantino <dconstan92@gmail.com>
2021-09-21 14:07:54 -0400

    refactor into single loop

--- src/components/BreadCrumbs.js
+++ src/components/BreadCrumbs.js
@@ -1,24 +1,19 @@
 import { Link } from "react-router-dom";
 
 function BreadCrumbs({ pathParts }) {
-  const linkParts = [];
-
   let currentLinkPath = "";
-  for (let i = 0; i < pathParts.length; i++) {
-    currentLinkPath += `/${pathParts[i]}`;
-    const currLink = (
-      <Link to={currentLinkPath}>
-        /<span className="px-1 text-blue-500 underline">{pathParts[i]}</span>
-      </Link>
-    );
-    linkParts.push(currLink);
-  }
 
   return (
     <div className="flex flex-wrap ml-2" data-testid="breadcrumbs">
-      {linkParts.map((link, idx) => (
-        <span key={idx}>{link}</span>
-      ))}
+      {pathParts.map((part, idx) => {
+        currentLinkPath += `/${part}`;
+
+        return (
+          <Link key={idx} to={currentLinkPath}>
+            /<span className="px-1 text-blue-500 underline">{part}</span>
+          </Link>
+        );
+      })}
     </div>
   );
 }
