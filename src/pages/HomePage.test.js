0da119dcb3d70d91705e33b9ea463b5500676d1c
Delia Constantino <dconstan92@gmail.com>
2021-09-21 11:21:45 -0400

    update HomePage tests

--- src/pages/HomePage.test.js
+++ src/pages/HomePage.test.js
@@ -7,46 +7,6 @@ const existingPath = "/teleport/lib";
 const nonexistentPath = "/directory/teleport/teleport";
 
 describe("HomePage Component", () => {
-  it("renders a table when errorMessage is false", () => {
-    const { getByRole } = render(
-      <MemoryRouter initialEntries={[existingPath]}>
-        <HomePage />
-      </MemoryRouter>
-    );
-    const table = getByRole("table");
-    expect(table).toBeTruthy();
-  });
-
-  it("does not render a table when errorMessage is true", () => {
-    const { queryByRole } = render(
-      <MemoryRouter initialEntries={[nonexistentPath]}>
-        <HomePage />
-      </MemoryRouter>
-    );
-    const table = queryByRole("table");
-    expect(table).toBeNull();
-  });
-
-  it("does not render the NoMatchFound alert when errorMessage is false", () => {
-    const { queryByText } = render(
-      <MemoryRouter initialEntries={[existingPath]}>
-        <HomePage />
-      </MemoryRouter>
-    );
-    const noMatch = queryByText("No matching directories found.");
-    expect(noMatch).toBeNull();
-  });
-
-  it("renders the NoMatchFound alert when errorMessage is true", () => {
-    const { getByText } = render(
-      <MemoryRouter initialEntries={[nonexistentPath]}>
-        <HomePage />
-      </MemoryRouter>
-    );
-    const noMatch = getByText("No matching directories found.");
-    expect(noMatch).toBeTruthy();
-  });
-
   it("finds data when the url pattern matches data in the directory", () => {
     const history = createMemoryHistory();
     history.push("/teleport/lib");
@@ -59,7 +19,7 @@ describe("HomePage Component", () => {
     expect(screen.getByText("newTeleport")).toBeTruthy();
   });
 
-  it("renders the noMatchFound component when the url pattern does not match data in the directory", () => {
+  it("does not find data when the url pattern does not match data in the directory", () => {
     const history = createMemoryHistory();
     history.push("/teleport/teleport/lib/hello");
     render(
@@ -69,5 +29,44 @@ describe("HomePage Component", () => {
     );
 
     expect(screen.getByText("No matching directories found.")).toBeTruthy();
+  });
+  it("renders a table when data is found", () => {
+    const { getByRole } = render(
+      <MemoryRouter initialEntries={[existingPath]}>
+        <HomePage />
+      </MemoryRouter>
+    );
+    const table = getByRole("table");
+    expect(table).toBeTruthy();
+  });
+
+  it("does not render a table when data is not found", () => {
+    const { queryByRole } = render(
+      <MemoryRouter initialEntries={[nonexistentPath]}>
+        <HomePage />
+      </MemoryRouter>
+    );
+    const table = queryByRole("table");
+    expect(table).toBeNull();
+  });
+
+  it("does not render 'No matching directories found.' when data is found", () => {
+    const { queryByText } = render(
+      <MemoryRouter initialEntries={[existingPath]}>
+        <HomePage />
+      </MemoryRouter>
+    );
+    const noMatch = queryByText("No matching directories found.");
+    expect(noMatch).toBeNull();
+  });
+
+  it("renders 'No matching directories found.' when data is not found", () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={[nonexistentPath]}>
+        <HomePage />
+      </MemoryRouter>
+    );
+    const noMatch = getByText("No matching directories found.");
+    expect(noMatch).toBeTruthy();
   });
 });
