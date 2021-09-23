51e334ffb148bb1d714be2a6187d19d2814a542f
Delia Constantino <dconstan92@gmail.com>
2021-09-22 22:03:44 -0400

    remove async from Table tests

--- src/components/table/Table.test.js
+++ src/components/table/Table.test.js
@@ -1,34 +1,35 @@
-import { act, fireEvent } from "@testing-library/react";
-import { renderWithRouter } from "../../test-utils";
+import { fireEvent, render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import Table from "./Table";
 
-const data = [
+const defaultTableData = [
   { name: "sampleDir", sizeKb: 0, type: "dir", items: [] },
   { name: "zebra.go", sizeKb: 520, type: "file" },
   { name: "another.go", sizeKb: 3320, type: "file" },
   { name: "aaaa.go", sizeKb: 3520, type: "file" },
 ];
 
-const location = {
-  location: { pathname: "/teleport/lib/newTeleport" },
-};
+const defaultLocation = { pathname: "/teleport/lib/newTeleport" };
 
-const initialEntries = ["/teleport/lib/newTeleport"];
+function renderTable(
+  tableProps = { data: defaultTableData, location: defaultLocation },
+  initialEntries = ["/teleport/lib/newTeleport"]
+) {
+  return render(
+    <MemoryRouter initialEntries={initialEntries}>
+      <Table {...tableProps} />
+    </MemoryRouter>
+  );
+}
 
 describe("Table Component", () => {
   it("renders the table header", () => {
-    const { getByTestId } = renderWithRouter(
-      <Table data={data} location={location} />,
-      initialEntries
-    );
+    const { getByTestId } = renderTable();
     expect(getByTestId("tableHeader")).toHaveTextContent("NameSize (Kb)");
   });
 
   it("renders the correct amount of table rows", () => {
-    const { getAllByTestId } = renderWithRouter(
-      <Table data={data} location={location} />,
-      initialEntries
-    );
+    const { getAllByTestId } = renderTable();
     const tableRowList = getAllByTestId("tableRow");
     expect(tableRowList.length).toBe(4);
   });
@@ -36,53 +37,36 @@ describe("Table Component", () => {
 
 describe("Sort Functionality", () => {
   it("renders an up arrow on page load", () => {
-    const { getByTitle } = renderWithRouter(
-      <Table data={data} location={location} />,
-      initialEntries
-    );
+    const { getByTitle } = renderTable();
     const svgTitle = getByTitle("arrowUp");
 
     expect(svgTitle).toBeTruthy();
   });
 
-  it("changes the arrow direction when clicked", async () => {
-    await act(async () => {
-      const { getByTitle } = renderWithRouter(
-        <Table data={data} location={location} />,
-        initialEntries
-      );
+  it("changes the arrow direction when clicked", () => {
+    const { getByTitle } = renderTable();
+    const arrowUp = getByTitle("arrowUp");
+    fireEvent.click(arrowUp);
+    const arrowDown = getByTitle("arrowDown");
 
-      const arrowUp = getByTitle("arrowUp");
-      await fireEvent.click(arrowUp);
-      const arrowDown = getByTitle("arrowDown");
-      expect(arrowDown).toBeTruthy();
-    });
+    expect(arrowDown).toBeTruthy();
   });
 
-  it("only shows name arrow when name is clicked", async () => {
-    await act(async () => {
-      const { queryByTestId } = renderWithRouter(
-        <Table data={data} location={location} />,
-        initialEntries
-      );
-      const name = queryByTestId("name");
-      await fireEvent.click(name);
-      const nameArrow = queryByTestId("nameArrow");
-      const sizeArrow = queryByTestId("sizeArrow");
+  it("only shows name arrow when name is clicked", () => {
+    const { queryByTestId } = renderTable();
+    const name = queryByTestId("name");
+    fireEvent.click(name);
+    const nameArrow = queryByTestId("nameArrow");
+    const sizeArrow = queryByTestId("sizeArrow");
 
-      expect(nameArrow).toBeTruthy();
-      expect(sizeArrow).toBeNull();
-    });
+    expect(nameArrow).toBeTruthy();
+    expect(sizeArrow).toBeNull();
   });
 
   it("only shows size arrow when size is clicked", () => {
-    const { queryByTestId } = renderWithRouter(
-      <Table data={data} location={location} />,
-      initialEntries
-    );
+    const { queryByTestId } = renderTable();
     const size = queryByTestId("size");
     fireEvent.click(size);
-
     const sizeArrow = queryByTestId("sizeArrow");
     const nameArrow = queryByTestId("nameArrow");
 
@@ -91,10 +75,7 @@ describe("Sort Functionality", () => {
   });
 
   it("sorts data by name in ascending order", () => {
-    const { queryByTestId, queryAllByTestId, getByTitle } = renderWithRouter(
-      <Table data={data} location={location} />,
-      initialEntries
-    );
+    const { queryByTestId, queryAllByTestId, getByTitle } = renderTable();
     const rowsInnerHTML = queryAllByTestId("nameValue").map(
       (val) => val.innerHTML
     );
@@ -102,63 +83,48 @@ describe("Sort Functionality", () => {
     const innerValues = [
       "aaaa.go",
       "another.go",
-      '<a class="text-blue-500 underline" href="/teleport/lib/undefined/sampleDir">sampleDir</a>',
+      '<a class="text-blue-500 underline" href="/teleport/lib/newTeleport/sampleDir">sampleDir</a>',
       "zebra.go",
     ];
 
     expect(rowsInnerHTML).toEqual(innerValues);
   });
 
-  it("sorts data by name in descending order", async () => {
-    await act(async () => {
-      const { queryByTestId, queryAllByTestId, getByTitle } = renderWithRouter(
-        <Table data={data} location={location} />,
-        initialEntries
-      );
-      const name = queryByTestId("name");
-      await fireEvent.click(name);
+  it("sorts data by name in descending order", () => {
+    const { queryByTestId, queryAllByTestId, getByTitle } = renderTable();
+    const name = queryByTestId("name");
+    fireEvent.click(name);
 
-      const rowsInnerHTML = queryAllByTestId("nameValue").map(
-        (val) => val.innerHTML
-      );
+    const rowsInnerHTML = queryAllByTestId("nameValue").map(
+      (val) => val.innerHTML
+    );
 
-      const innerValues = [
-        "zebra.go",
-        '<a class="text-blue-500 underline" href="/teleport/lib/undefined/sampleDir">sampleDir</a>',
-        "another.go",
-        "aaaa.go",
-      ];
+    const innerValues = [
+      "zebra.go",
+      '<a class="text-blue-500 underline" href="/teleport/lib/newTeleport/sampleDir">sampleDir</a>',
+      "another.go",
+      "aaaa.go",
+    ];
 
-      expect(rowsInnerHTML).toEqual(innerValues);
-    });
+    expect(rowsInnerHTML).toEqual(innerValues);
   });
 
-  it("sorts data by size in ascending order", async () => {
-    await act(async () => {
-      const { queryByTestId, queryAllByTestId, getByTitle } = renderWithRouter(
-        <Table data={data} location={location} />,
-        initialEntries
-      );
-      const size = queryByTestId("size");
-      await fireEvent.click(size);
-      const rows = queryAllByTestId("sizeValue").map((val) => val.innerHTML);
+  it("sorts data by size in ascending order", () => {
+    const { queryByTestId, queryAllByTestId, getByTitle } = renderTable();
+    const size = queryByTestId("size");
+    fireEvent.click(size);
+    const rows = queryAllByTestId("sizeValue").map((val) => val.innerHTML);
 
-      expect(rows).toEqual(["-", "520", "3320", "3520"]);
-    });
+    expect(rows).toEqual(["-", "520", "3320", "3520"]);
   });
 
-  it("sorts data by size in descending order", async () => {
-    await act(async () => {
-      const { queryByTestId, queryAllByTestId, getByTitle } = renderWithRouter(
-        <Table data={data} location={location} />,
-        initialEntries
-      );
-      const size = queryByTestId("size");
-      await fireEvent.click(size);
-      await fireEvent.click(size);
-      const rows = queryAllByTestId("sizeValue").map((val) => val.innerHTML);
+  it("sorts data by size in descending order", () => {
+    const { queryByTestId, queryAllByTestId, getByTitle } = renderTable();
+    const size = queryByTestId("size");
+    fireEvent.click(size);
+    fireEvent.click(size);
+    const rows = queryAllByTestId("sizeValue").map((val) => val.innerHTML);
 
-      expect(rows).toEqual(["3520", "3320", "520", "-"]);
-    });
+    expect(rows).toEqual(["3520", "3320", "520", "-"]);
   });
 });
